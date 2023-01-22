f4788e65180943c9c50791c62363b28f035f649c
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2023-01-22 12:39:23 +0330

    Create Sutaible Directories

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -2,9 +2,23 @@
 
 declare(strict_types = 1);
 
+use App\Controller\BasketController;
 use App\Controllers\HomeController;
+use App\Controllers\ProductController;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
     $app->get('/', [HomeController::class, 'index']);
+
+    $app->group('/product', function (RouteCollectorProxy $categories) {
+        $categories->get('', [ProductController::class, 'index']);
+        $categories->post('', [ProductController::class, 'store']);
+        $categories->delete('/{id:[0-9]+}', [ProductController::class, 'delete']);
+        $categories->get('/{id:[0-9]+}', [ProductController::class, 'get']);
+        $categories->post('/{id:[0-9]+}', [ProductController::class, 'update']);
+    });
+    $app->group('/basket', function (RouteCollectorProxy $categories) {
+        $categories->get('/{id:[0-9]+}', [BasketController::class, 'index']);
+    });
 };
